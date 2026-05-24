9b132766dc1308a922137bde10f2234a1777f360
Russell  Lack <62655965+RussellLack@users.noreply.github.com>
2026-05-25 00:33:25 +0200

    Add GoogleAnalytics component for tracking

---
+++ src/app/src/components/src/components/src/components/GoogleAnalytics.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Script from "next/script";
+
+export function GoogleAnalytics() {
+  const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
+  if (!measurementId) return null;
+
+  return (
+    <>
+      <Script id="ga-consent-default" strategy="beforeInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('consent', 'default', {
+            ad_storage: 'denied',
+            ad_user_data: 'denied',
+            ad_personalization: 'denied',
+            analytics_storage: 'denied',
+            wait_for_update: 500
+          });
+          gtag('set', 'ads_data_redaction', true);
+          gtag('set', 'url_passthrough', true);
+        `}
+      </Script>
+
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+        strategy="afterInteractive"
+      />
+
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${measurementId}', {
+            anonymize_ip: true,
+            send_page_view: true
+          });
+        `}
+      </Script>
+    </>
+  );
+}
